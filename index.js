50d2e3d9e85edc1d72bb0978fadb23618ede3591
Robert Gaspar <gaspar.robert7@gmail.com>
2021-11-01 22:04:26 -0500

    commit tree 30f85f3e3ca90032bd6c41c98041fe7db31ac180

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
+const num1 = 2
+const num2 = 31
+const multiply = num1*num2
 
-// Write your code here
+const random = Math.floor(Math.random()*33)
+const num3 = 6
+const num4 = 16 
+const mod = num4 % num3
+const max = Math.max(5, 10, 15, 20)
